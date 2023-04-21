30749c8fe0824408d4975d9e7cc9eedb2d88c667
hharitha97 <55691974+hharitha97@users.noreply.github.com>
2023-04-21 12:14:10 +0530

    Create script.js
    Adding script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+(() => {
+
+    const sec = 1000,
+      min = sec * 60,
+      hour = min * 60,
+      day = hour * 24;
+  
+    const end = new Date("May 21, 2023 11:57:00").getTime();
+  
+    const int = setInterval(() => {
+      const current = new Date().getTime();
+      const remaining = end - current;
+      document.getElementById("days").innerText = Math.floor(remaining / day);
+      document.getElementById("hours").innerText = Math.floor( (remaining % day) / hour );
+      document.getElementById("minutes").innerText = Math.floor( (remaining % hour) / min );
+      document.getElementById("seconds").innerText = Math.floor( (remaining % min) / sec );          
+    }, 1000);
+  
+  })();
