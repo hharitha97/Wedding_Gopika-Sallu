e4732757ff1d457173d4dc38f0e2cb2875a22350
Akhil.VS <121923755+vsakhil07@users.noreply.github.com>
2023-04-23 17:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,11 @@
       document.getElementById("hours").innerText = Math.floor( (remaining % day) / hour );
       document.getElementById("minutes").innerText = Math.floor( (remaining % hour) / min );
       document.getElementById("seconds").innerText = Math.floor( (remaining % min) / sec );          
-    }, 1000);    
+    }, 1000);
+  setTimeout(function() {
+  document.body.style.opacity = '0';
+  setTimeout(function() {
+    window.location.href = 'https://vsakhil07.github.io/Wedding-Invitation_Gopika-Sallu/';
+  }, 1000); // Wait for 1 second before redirecting
+}, 5000); // Wait for 5 seconds before fading out
 })();
